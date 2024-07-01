0eddf518e0883222aa76e631b3ba3813b7fdb62a
Nicholas <nick.orsi1992@gmail.com>
2024-07-01 16:43:59 -0700

    Built out the controlled SearchForm component.

--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SearchForm() {
+
+type SearchFormProps = {
+    // initialSearchTerm: string,
+    onSearch: ((param:string) => void),
+}
+/**
+ * SearchForm component used to hold a simple text input and button.
+ *
+ * Takes prop to {initialSearchTerm, onSearch} that is the text in the input
+ * and the function to run when the user searches.
+ *
+ * {JobList, CompanyList} -> SearchForm
+ */
+function SearchForm({onSearch}: SearchFormProps) {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    function handleSearch(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        e.preventDefault();
+        setSearchTerm(searchTerm.trim());
+        onSearch(searchTerm.trim());
+    }
     return (
         <div>
-            SearchForm Componenet
+            <form action="GET" >
+                <input
+                    type="text"
+                    placeholder="Enter a search term..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button type="submit" onClick={handleSearch}>Search</button>
+            </form>
         </div>
     )
 }

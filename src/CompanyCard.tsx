49c6f6b20458fe4ca7f88426eeeb201ee463c302
Nicholas <nick.orsi1992@gmail.com>
2024-07-01 18:31:46 -0700

    Added content to the CompanyCard component.

--- src/CompanyCard.tsx
+++ src/CompanyCard.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { CompanyDataFromAPI } from "./CompanyList";
 
-function CompanyCard() {
+/**
+ * CompanyCard component that renders information about the company.
+ *
+ * Takes props {handle, name, description, numEmployees, logoURL} as props
+ *
+ * CompanyList -> CompanyCard
+ */
+function CompanyCard(
+    {handle, name, description, logoURL}: CompanyDataFromAPI
+) {
     return (
-        <div>CompanyCard Component</div>
+        <a href={`companies/${handle}`}>
+            <div className="company-card">
+                <div className="company-titles">
+                    <p>{ name }</p>
+                    <img src={`${logoURL}`} alt="company logo" />
+                </div>
+                <div className="company-info">
+                    <p>{ description }</p>
+                </div>
+            </div>
+        </a>
     )
 }
 

f74414025d4caa4780b2059162e58f382f5c280b
Nicholas <nick.orsi1992@gmail.com>
2024-07-09 17:45:57 -0700

    Added docstring and adjusted url param variable name to better match the resource data name.

--- src/RoutesList.tsx
+++ src/RoutesList.tsx
@@ -5,12 +5,20 @@ import { Company } from "./Company"
 import { JobList } from "./JobList"
 import NotFound from "./NotFound"
 
+/**
+ * RoutesList componenet relying on the react-dom-router dependency.
+ *
+ * Maps componenets to relevant paths on the client side to mimic the
+ * URL behavior of traditional html rendering sites.
+ *
+ * App -> RoutesList -> {Home, CompanyList, JobList, NotFound}
+ */
 function RoutesList() {
     return (
         <Routes>
             <Route path='/' element={ <Home/> }></Route>
             <Route path='companies' element={ <CompanyList/> }></Route>
-            <Route path='companies/:companyId' element={ <Company/> }></Route>
+            <Route path='companies/:companyHandle' element={ <Company/> }></Route>
             <Route path='jobs' element={ <JobList/> }></Route>
             <Route path='*' element={ <NotFound/>}></Route>
         </Routes>

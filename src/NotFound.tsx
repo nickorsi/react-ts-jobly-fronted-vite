cfd8955505e76aee4c687b398c67e6fb88717121
Nicholas <nick.orsi1992@gmail.com>
2024-07-09 17:45:02 -0700

    Added docstring and further fleshed out contents.

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import "./NotFound.css";
 
+/**
+ * NotFound componenet with generic message for users that have reached a URL
+ * that is not mapped to a specific path.
+ *
+ * RoutesList -> NotFound
+ */
 function NotFound() {
     return (
-        <>
-            <h2>Sorry!</h2>
+        <div className="not-found card">
+            <h2>Sorry! 😅</h2>
             <p>Looks like that page doesn't exist!</p>
-            <p>Instead why don't you look at the rest of our site!</p>
-        </>
+            <p>Please check out the rest of our site with the links above!</p>
+        </div>
     )
 }
 

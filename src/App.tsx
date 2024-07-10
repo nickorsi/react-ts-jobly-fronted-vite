b94630a9dce4e36bf94c0b24a3ef1744bf0f2d33
Nicholas <nick.orsi1992@gmail.com>
2024-07-09 17:37:23 -0700

    Minor adjustments including styling import, docstring, and adding a div wrapper.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import "./App.css";
 import NavBar from './NavBar'
 import RoutesList from './RoutesList'
 import { BrowserRouter } from 'react-router-dom'
 
+/**
+ * The main App component for Jobly.
+ *
+ * Renders the NavBar componenet and the RoutesList component.
+ *
+ * index.tsx -> App -> {NavBar, RoutesList}
+ */
 function App() {
 
   return (
     <BrowserRouter>
       <NavBar></NavBar>
-      <RoutesList></RoutesList>
+      <div className='page-contents'>
+        <RoutesList></RoutesList>
+      </div>
     </BrowserRouter>
   )
 }

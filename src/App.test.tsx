c80c00e5bcfab410e6b63bb440d7a9036be63989
Nicholas <nick.orsi1992@gmail.com>
2024-08-15 19:15:59 -0700

    Continued writing tests for the App component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,12 @@ import userEvent from "@testing-library/user-event";
 import { JoblyApi } from './api.ts';
 import App from './App.tsx';
 
+/**
+ * Something wrong with test config, uncomment line below to get intellisense to
+ * recognize the extended expect methods, comment out for tests to run properly.
+*/
+// import "@testing-library/jest-dom";
+
 const mockedGetJobs = vi.spyOn(JoblyApi, "getJobs");
 const mockedGetCompanies = vi.spyOn(JoblyApi, "getCompanies");
 const mockedGetCompany = vi.spyOn(JoblyApi, "getCompany");
@@ -60,21 +66,21 @@ it("can be navigated by the user", async function () {
           name: 'Company 1',
           description: "Description 1",
           numEmployees: 1,
-          logoUrl: 'http://c1.img',
+          logoUrl: 'http//c1.img',
         },
         {
           handle: 'c2',
           name: 'Company 2',
           description: "Description 2",
           numEmployees: 2,
-          logoUrl: 'http://c2.img',
+          logoUrl: 'http//c2.img',
         },
         {
           handle: 'c3',
           name: 'Company 3',
           description: "Description 3",
           numEmployees: 3,
-          logoUrl: 'http://c3.img',
+          logoUrl: 'http//c3.img',
         },
       ]));
     mockedGetCompany.mockReturnValue(Promise.resolve({
@@ -82,7 +88,7 @@ it("can be navigated by the user", async function () {
         name: 'Company 1',
         description: "Description 1",
         numEmployees: 1,
-        logoUrl: 'http://c1.img',
+        logoUrl: 'http//c1.img',
         jobs: [
           {
             id: 1,
@@ -94,20 +100,33 @@ it("can be navigated by the user", async function () {
         ]
       }));
 
-    const { container } = render(<App />)
+    const { container, debug } = render(<App />)
 
     const user = userEvent.setup();
 
     // Navigating to jobs page
     await user.click(screen.getByTestId("jobs-nav-button"));
-    // await user.click(container.querySelector("a [data-testid=jobs-nav-button]") as Element);
 
     expect(container.querySelectorAll(".job.card").length).toEqual(3);
     expect(container.querySelectorAll(".job.card")[0]).toContainHTML("Job 1");
     expect(container.querySelectorAll(".job.card")[1]).toContainHTML("Job 2");
     expect(container.querySelectorAll(".job.card")[2]).toContainHTML("Job 3");
 
-    // TODO: Add tests for navigation to companies and company page.
+    // Navigating to companies page
+    await user.click(screen.getByTestId("companies-nav-button"));
 
+    expect(container.querySelectorAll(".company.card").length).toEqual(3);
+    expect(container.querySelectorAll(".company.card")[0]).toContainHTML("Company 1");
+    expect(container.querySelectorAll(".company.card")[1]).toContainHTML("Company 2");
+    expect(container.querySelectorAll(".company.card")[2]).toContainHTML("Company 3");
 
+    // Navigating to company page
+    await user.click(container.querySelector("img[src='http//c1.img']") as Element);
+
+    expect(container.querySelectorAll(".company-container").length).toEqual(1);
+    expect(container.querySelector(".company-container")).toContainHTML("Company 1");
+    expect(container.querySelectorAll(".job.card").length).toEqual(1);
+    expect(container.querySelector(".job.card")).toContainHTML("Job 1");
+
+  // TODO: Write tests for when API requests return error
 })

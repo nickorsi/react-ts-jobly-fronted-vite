6f881d76a2d40e6d49a95b0ef65abb2279eb29d8
Nicholas <nick.orsi1992@gmail.com>
2024-09-17 20:47:51 -0700

    Add tests for the rest of the routes.

--- src/nav/RoutesList.test.tsx
+++ src/nav/RoutesList.test.tsx
@@ -1,6 +1,7 @@
 import {describe, expect, it} from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import { mockedGetCompany } from "../setupTests.ts";
 
 import RoutesList from "./RoutesList.tsx";
 
@@ -49,43 +50,63 @@ describe("Test Individual Routes", function() {
         );
 
         await waitFor(() => {
-            screen.debug();
             expect(
                 screen.getByText("All Companies")
             ).toBeInTheDocument();
         });
     });
-    it("renders the company page correctly", function() {
+    it("renders the company page correctly", async function() {
+        mockedGetCompany.mockResolvedValue({
+            handle: "c1",
+            name: "Company 1",
+            description: "Description 1",
+            numEmployees: 1,
+            logoUrl: "http//c1.img",
+            jobs: [
+              {
+                id: 1,
+                title: "Job 1",
+                salary: 1,
+                equity: "1",
+                companyName: "Company 1",
+              }
+            ]
+        });
+
         render(
             <MemoryRouter initialEntries={["/companies/c1"]}>
                 <RoutesList ></RoutesList>
             </MemoryRouter>
         );
 
-        expect(
-            screen.getByText("All the jobs in one convenient place.")
-        ).toBeInTheDocument();
+        await waitFor(() => {
+            expect(
+                screen.getByText("Company 1")
+            ).toBeInTheDocument();
+        });
     });
-    it("renders the jobs page correctly", function() {
+    it("renders the jobs page correctly", async function() {
         render(
-            <MemoryRouter >
+            <MemoryRouter initialEntries={["/jobs"]}>
                 <RoutesList ></RoutesList>
             </MemoryRouter>
         );
 
-        expect(
-            screen.getByText("All the jobs in one convenient place.")
-        ).toBeInTheDocument();
+        await waitFor(() => {
+            expect(
+                screen.getByText("All Jobs")
+            ).toBeInTheDocument();
+        });
     });
     it("renders the 404 page correctly", function() {
         render(
-            <MemoryRouter >
+            <MemoryRouter initialEntries={["/test"]}>
                 <RoutesList ></RoutesList>
             </MemoryRouter>
         );
 
         expect(
-            screen.getByText("All the jobs in one convenient place.")
+            screen.getByText("Looks like that page doesn't exist!")
         ).toBeInTheDocument();
     });
 });

3f4446444b99b5d109e9e508b99268a6b2c8fdc9
Nicholas <nick.orsi1992@gmail.com>
2024-09-17 20:39:59 -0700

    Mock the methods used by the frontend to make the api calls.
    Added this to the setup to avoid unnecessary API calls in the testing by accident.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,10 @@
 // This done here will be available in all test files.
-import { expect, afterEach } from "vitest";
+import { vi, expect, afterEach } from "vitest";
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
+import { JoblyApi } from "./api/api.ts";
+
+
 // import '@testing-library/jest-dom';
 // Have extending the matchers on the expect from vitest, but intellisense does
 // not recognize the extended matchers UNLESS line 5 is uncommented. BUT this
@@ -10,3 +13,9 @@ import * as matchers from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
 afterEach(cleanup);
+
+const mockedGetJobs = vi.spyOn(JoblyApi, "getJobs");
+const mockedGetCompanies = vi.spyOn(JoblyApi, "getCompanies");
+const mockedGetCompany = vi.spyOn(JoblyApi, "getCompany");
+
+export { mockedGetJobs, mockedGetCompanies, mockedGetCompany };

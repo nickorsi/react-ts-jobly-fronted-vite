9b7fb698770a01eb43483debd39e5abc7b3d98dc
Nicholas <nick.orsi1992@gmail.com>
2024-09-18 17:54:38 -0700

    Add coverage to project.
    Had to update dependencies in order to properly run the coverage script.
    Updated the config file as well and the gitignore file to exclude the coverage folder.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: 'v8'
+    },
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
     clearMocks: true,

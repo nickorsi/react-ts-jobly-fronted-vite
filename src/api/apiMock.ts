6470fc98cc2e7fdb68f097ece9765b75d4285823
Nicholas <nick.orsi1992@gmail.com>
2024-10-20 13:22:21 -0700

    Modify file to avoid warning/errors on build.

--- src/api/apiMock.ts
+++ src/api/apiMock.ts
@@ -16,15 +16,17 @@ function mockAPI(
   status:number = 200
 ) {
 
-mockServer.use(http[method](path, ({request}) => {
-    // Added lines 24-27 to remove stderror from msw for redundent query-parameters
-    // being present in the passed in path parameter. These queries aren't
-    // really relied on in the return value because the return data is already
-    // mocked according to the passed in query.
-    const url = new URL(request.url);
+mockServer.use(http[method](path, () => {
+    // At one point received a remove stderror from msw for redundent
+    // query-parameters being present in the passed in path parameter.
+    // These queries aren't really relied on in the return value because
+    // the return data is already mocked according to the passed in query.
+    // At a later time it appears this stderror is no longer occuring, but
+    // left this code in case it pop up again.
+    // const url = new URL(request.url); // request object needs to be destructured in the param of the arrow function.
 
-    const nameLike = url.searchParams.get('nameLike');
-    const title = url.searchParams.get('title');
+    // const nameLike = url.searchParams.get('nameLike');
+    // const title = url.searchParams.get('title');
 
     return HttpResponse.json(data, {status: status})
     })
